d13548c4a90742cecddef330a71ece378a379825
Ardi Firmansyah <ardi.firmansyah@wartek.id>
2024-08-30 22:36:08 +0700

    fix: add suspense

--- src/app/donation/page.tsx
+++ src/app/donation/page.tsx
@@ -1,5 +1,11 @@
+import { Suspense } from "react";
+
 import Donation from "@/features/Donation";
 
 export default function Page() {
-  return <Donation />;
+  return (
+    <Suspense>
+      <Donation />
+    </Suspense>
+  );
 }
